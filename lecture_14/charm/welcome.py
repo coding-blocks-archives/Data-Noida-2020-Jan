789002a0b1a34043c9bb3ecf23a1718e5245f495
KeenWarrior <anujgargcse@gmail.com>
2020-03-15 16:41:42 +0530

    l 13

--- lecture_14/charm/welcome.py
+++ lecture_14/charm/welcome.py
@@ -2,17 +2,31 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 while True:
 
     retval, image = cap.read()
 
     if retval:
-        # cv2.imshow("my window", image)
 
-        cut = image[:200, :200]
-        cv2.imshow("my window", cut)
+        faces = classifier.detectMultiScale(image)
 
-    key = cv2.waitKey(1)
+        print(faces)
+
+        if len(faces) > 0:
+            face = faces[0]
+            x, y, w, h = face
+
+            cut = image[y:y+h, x:x+w]
+
+            fix_cut = cv2.resize(cut, (500, 500))
+
+            gray = cv2.cvtColor(fix_cut, cv2.COLOR_BGR2GRAY)
+
+            cv2.imshow("my window", gray)
+
+    key = cv2.waitKey(100)
 
     if ord("b") == key:
         break
